6a1d00e4186062d2002546ead5ec904bac9719d2
Stranger DaNeK <black.danek@gmail.com>
2013-12-09 17:49:32 +0400

    testing

--- backend/pleer.class.php
+++ backend/pleer.class.php
@@ -296,15 +296,16 @@
 		 */
 		private function request($url, $method = 'GET', $postfields = array(), $auth = false)
 		{
-			curl_setopt_array($this->ch, array(
-				CURLOPT_USERAGENT       => 'MPD/1.0 (+StrangeMPD))',
-				CURLOPT_RETURNTRANSFER  => true,
-				CURLOPT_SSL_VERIFYPEER  => false,
-				CURLOPT_USERPWD			=> (!is_null($this->username) && !is_null($this->password) && $auth)? $this->username.':'.$this->password : false,
-				CURLOPT_POST            => ($method == 'POST'),
-				CURLOPT_POSTFIELDS      => $postfields,
-				CURLOPT_URL             => $url
-			));
+			$curl_setopt = array();
+				$[CURLOPT_USERAGENT] = 'PHPAPI/1.0 (+StrangeMPD))';
+				$[CURLOPT_RETURNTRANSFER] = true;
+				$[CURLOPT_SSL_VERIFYPEER] = false;
+			if(!is_null($this->username) && !is_null($this->password) && $auth)
+				$[CURLOPT_USERPWD] = $this->username.':'.$this->password;
+				$[CURLOPT_POST] = ($method == 'POST');
+				$[CURLOPT_POSTFIELDS] = $postfields;
+				$[CURLOPT_URL] = $url;
+			curl_setopt_array($this->ch, $curl_setopt);
         	return curl_exec($this->ch);
     	}
 	}
